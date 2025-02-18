463213375e4bdaf960bb58245af17ac12a74a801
santosh-316 <santoshch1999@gmail.com>
2025-02-18 16:45:51 +0530

    updated

--- program2.java
+++ program2.java
@@ -4,7 +4,7 @@ public class program2 {
 	//   List<Integer> number = Arrays.asList(1,2,3,4,5,6);
       //number.stream().forEach(s -> System.out.println(s));
       //number.stream().filter(s -> s%2==0).forEach(s->System.out.println(s));
-	  
+	  //sample
 
     }
     
